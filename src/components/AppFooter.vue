6859a7772e489e2403aefe4808dd086e98f4114e
NicoCodesCode <nicolasrmz08@gmail.com>
2026-05-02 21:03:15 -0500

    Put footer in its own component

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,5 @@
+<template>
+  <footer class="px-8 py-5 border-t border-zinc-100">
+    <p class="text-xs text-zinc-400 text-center tracking-wide">© 2026 YaVoy</p>
+  </footer>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,6 +6,7 @@ import EmailAuthDialog from '@/components/EmailAuthDialog.vue'
 import { useAuth } from '@/composables/useAuth'
 import LoadingScreen from '@/components/LoadingScreen.vue'
 import NavBar from '@/components/NavBar.vue'
+import AppFooter from '@/components/AppFooter.vue'
 
 const {
   step,
@@ -31,9 +32,7 @@ const {
       <ExploreView v-if="currentUser" :currentUser />
       <LandingView v-else @open="open" />
 
-      <footer class="px-8 py-5 border-t border-zinc-100">
-        <p class="text-xs text-zinc-400 text-center tracking-wide">© 2026 YaVoy</p>
-      </footer>
+      <AppFooter />
     </template>
   </div>
 
