f119d425860552d025984fe7fe243a738efabd73
NicoCodesCode <nicolasrmz08@gmail.com>
2026-05-06 19:20:59 -0500

    Move header and footer from Home to App

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,26 +5,13 @@ import AuthSelectorDialog from '@/components/AuthSelectorDialog.vue'
 import EmailAuthDialog from '@/components/EmailAuthDialog.vue'
 import OnboardingDialog from '@/components/OnboardingDialog.vue'
 import { useAuth } from '@/stores/auth'
-import LoadingScreen from '@/components/LoadingScreen.vue'
-import NavBar from '@/components/NavBar.vue'
-import AppFooter from '@/components/AppFooter.vue'
 
 const authStore = useAuth()
 </script>
 
 <template>
-  <div class="min-h-screen bg-white flex flex-col">
-    <LoadingScreen v-if="authStore.isLoading" />
-
-    <template v-else>
-      <NavBar />
-
-      <ExploreView v-if="authStore.currentUser" />
-      <LandingView v-else />
-
-      <AppFooter />
-    </template>
-  </div>
+  <ExploreView v-if="authStore.currentUser" />
+  <LandingView v-else />
 
   <AuthSelectorDialog />
   <EmailAuthDialog />
