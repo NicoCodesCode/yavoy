83e8b36b06bcdff23573a9a39bc37afce738f5c0
NicoCodesCode <nicolasrmz08@gmail.com>
2026-05-06 19:20:15 -0500

    Move header and footer to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,21 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import AppFooter from './components/AppFooter.vue'
+import NavBar from './components/NavBar.vue'
+import LoadingScreen from '@/components/LoadingScreen.vue'
+import { useAuth } from './stores/auth'
+
+const authStore = useAuth()
+</script>
 
 <template>
-  <RouterView />
+  <div class="min-h-screen bg-white flex flex-col">
+    <LoadingScreen v-if="authStore.isLoading" />
+    <template v-else>
+      <NavBar />
+      <RouterView />
+      <AppFooter />
+    </template>
+  </div>
 </template>
 
 <style scoped></style>
