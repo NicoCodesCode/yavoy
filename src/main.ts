8f0917f6d3036c726faee40671059011c83eee92
NicoCodesCode <nicolasrmz08@gmail.com>
2026-05-06 19:19:23 -0500

    Change theme to material

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import PrimeVue from 'primevue/config'
-import Aura from '@primeuix/themes/aura'
+import Material from '@primeuix/themes/material'
 import 'primeicons/primeicons.css'
 import '@/main.css'
 import { createPinia } from 'pinia'
@@ -11,7 +11,7 @@ const app = createApp(App)
 
 app.use(router)
 
-app.use(PrimeVue, { theme: { preset: Aura } })
+app.use(PrimeVue, { theme: { preset: Material } })
 app.use(createPinia())
 
 app.mount('#app')
