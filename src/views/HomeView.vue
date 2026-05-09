07d299d6a589ceef92a6fffdc68fea826cb32505
NicoCodesCode <nicolasrmz08@gmail.com>
2026-05-09 17:41:23 -0500

    Fix components path

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
 import ExploreView from './ExploreView.vue'
 import LandingView from './LandingView.vue'
-import AuthSelectorDialog from '@/components/AuthSelectorDialog.vue'
-import EmailAuthDialog from '@/components/EmailAuthDialog.vue'
-import OnboardingDialog from '@/components/OnboardingDialog.vue'
+import AuthSelectorDialog from '@/components/auth/AuthSelectorDialog.vue'
+import EmailAuthDialog from '@/components/auth/EmailAuthDialog.vue'
+import OnboardingDialog from '@/components/auth/OnboardingDialog.vue'
 import { useAuth } from '@/stores/auth'
 
 const authStore = useAuth()
